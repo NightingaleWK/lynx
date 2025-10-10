d38940b4c50bce7159021333441af269fd4b7f37
NightingaleWK <djsywxsj@outlook.com>
2025-10-10 15:40:22 +0800

    容器化部署改造

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class TrustProxies extends Middleware
+class TrustProxies
 {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return $next($request);
+    }
+
     /**
      * The trusted proxies for this application.
      *
