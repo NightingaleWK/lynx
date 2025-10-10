8d2ff3278e57a26f542577e611629b20b1b67941
NightingaleWK <djsywxsj@outlook.com>
2025-10-10 17:24:49 +0800

    信任代理调整

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,9 @@ class AppServiceProvider extends ServiceProvider
         // 解决了在反向代理后，Laravel 错误地将内部端口（如 :80）
         // 附加到 HTTPS 链接上的问题。
         // =================================================================
-        URL::forceScheme('https');
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
 
         // 注册OrderItem观察者
         OrderItem::observe(OrderObserver::class);
